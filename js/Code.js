44c00dcf40bca57f510c5c6e06a701503eed380e
cjcb0607 <91701797+cjcb0607@users.noreply.github.com>
2022-02-23 21:17:28 -0600

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,24 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    fill("white")
+    textSize(15)
+    text("N O T F N I C U", 700,150)
+    fill("lightblue")
+    text("Hint: is void on C#  !!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text(" T A S A D E B A ", 400,250)
+    fill("lightblue")
+    text("Hint: google firebase !!", 400,270)
+}
